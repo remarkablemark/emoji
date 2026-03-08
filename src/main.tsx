f2069281cf8b8d92188cbdd99d2a95ee96a22aa7
Mark <mark@remarkablemark.org>
2026-03-08 16:38:22 -0400

    refactor: rename index.css to style.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import './index.css';
+import './style.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
